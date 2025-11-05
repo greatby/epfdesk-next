74f4de88bb4e4409481c654b84e72428bf68fca0
greatby <gills_halogen2i@icloud.com>
2025-11-05 10:37:15 +0530

    Minor code changes in hero section

--- src/components/PerformanceSection.jsx
+++ src/components/PerformanceSection.jsx
@@ -4,7 +4,7 @@ import { TiArrowRepeat } from "react-icons/ti";
 export const PerformanceSection = ({ data }) => {
   return (
     <>
-      <section className="flex my-8 flex-col items-center justify-center px-6 pt-20 text-center">
+      <section className="flex mt-8 flex-col items-center justify-center px-6 pt-20 text-center">
         {/* Label */}
         <div className="flex items-center gap-2 mb-3">
           <div className=" text-[#001f1f] flex items-center gap-3 font-semibold px-3 py-1 rounded-full text-[17px]">
@@ -29,7 +29,7 @@ export const PerformanceSection = ({ data }) => {
         </h3>
 
         {/* Subtitle */}
-        <p className="text-[#001f1f] font-normal mt-4 mb-8 max-w-xl text-[1.2rem]">
+        <p className="text-[#001f1f] font-normal mt-4 mb-6 max-w-xl text-[1.2rem]">
           {data?.description}
         </p>
 
@@ -164,7 +164,7 @@ export const PerformanceSection = ({ data }) => {
 
         {/* Card */}
       </section>
-      <div className="my-16 relative p-1 rounded-3xl w-full max-w-5xl mx-auto">
+      <div className=" relative p-1 rounded-3xl w-full max-w-5xl mx-auto">
         <div className="p-6 m-2 transition  rounded-[20px] ">
           {/*shadow-[inset_0_10px_16px_-10px_#ffffff0f,0_8px_8px_#001f1f0f,0_4px_4px_#001f1f0f,0_2px_2px_#001f1f0f,0_0_1px_#001f1f52,inset_0_-2px_1px_#001f1f3d,inset_0_1px_1px_#cdface14]*/}
           <div className="grid grid-rows-1 text-left ">
